ad56a7972bfc5b13b502552c602e73edd951d3e1
Berfin Korkmaz <sumeyyeberfinkorkmaz@gmail.com>
2024-09-20 15:56:01 +0300

    first commit

--- src/main/java/com/example/student/Controllers/EnrollmentController.java
+++ src/main/java/com/example/student/Controllers/EnrollmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/dersKayit")
+@RequestMapping("/Enrollment")
 public class EnrollmentController {
     private final EnrollmentService enrollmentService;
     @Autowired
